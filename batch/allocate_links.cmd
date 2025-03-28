e7894baf38754a81430f73ae7a6bd3a306531cf2
Chris Jacobs-Crisioni <chris@jacobs-crisioni.nl>
2025-03-28 12:08:59 +0100

    considerable improvements to the model, ensuring consistency with recent GeoDMS versions
    start with universal cost estimates (only ex-post now)
    
    fixes to errors in investments overview

--- batch/allocate_links.cmd
+++ batch/allocate_links.cmd
@@ -1,8 +1,14 @@
-C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms ChoiceSet/SelZones/Alternatives/ResultsCollection/allLinks 
+rem C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms ChoiceSet/SelZones/Alternatives/ResultsCollection/allLinks 
 
-C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms LinkBenefits/calcBenefits/calcAllTrigger/CalcAll
+rem C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms LinkBenefits/calcBenefits/calcAllTrigger/CalcAll
 
-C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms InvestmentDecision/SimpleDecisionEvaluator/Ran
+rem only recalculate base values
+C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms LinkBenefits/calcBenefits/calcAllTrigger/Store_Operators
+
+rem only simple evaluation (random operator!!!)
+rem C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms InvestmentDecision/SimpleDecisionEvaluator/Ran
+
+C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms InvestmentDecision/Ran
 
 rem pause
 
--- batch/repeat_analysis_scenarioloop.cmd
+++ batch/repeat_analysis_scenarioloop.cmd
@@ -2,7 +2,8 @@ set pf="Program Files"
 set config=F:\ProjDir\TransportLinkScanner\cfg
 set geodmsversion=GeoDMS15.0.1
 
-set scenariolist=calib_elastic_speed_inc_local_speed, calib_notelastic_speed_inc_local_speed
+set scenariolist=calib_elastic_local_speed calib_notelastic_local_speed calib_elastic_speed_inc_local_speed calib_notelastic_speed_inc_local_speed calib_elastic_rnd_speed_inc_local_speed calib_elastic_rnd_shortest_speed_inc_local_speed calib_elastic_ms0_speed_inc_local_speed calib_elastic_ms1_speed_inc_local_speed calib_elastic_ms2_speed_inc_local_speed
+rem set scenariolist=calib_elastic_rnd_speed_inc_local_speed calib_elastic_rnd_shortest_speed_inc_local_speed calib_elastic_ms0_speed_inc_local_speed calib_elastic_ms1_speed_inc_local_speed calib_elastic_ms2_speed_inc_local_speed
 
 
 for %%s in (%scenariolist%) do (
@@ -13,7 +14,7 @@ for %%s in (%scenariolist%) do (
 	rem pause
 	
 	for /l %%x in (0, 1, 100) do (
-	 start /wait allocate_links
+	 start /wait /min allocate_links
 	)
 	
 )
