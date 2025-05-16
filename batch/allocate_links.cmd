744e7f8636f4e68d9e9841a1edfbfc41e38b9518
Chris Jacobs-Crisioni <chris@jacobs-crisioni.nl>
2025-05-16 14:42:34 +0200

    many small fixes driven by found data issues

--- batch/allocate_links.cmd
+++ batch/allocate_links.cmd
@@ -3,7 +3,8 @@ rem C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms ChoiceS
 rem C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms LinkBenefits/calcBenefits/calcAllTrigger/CalcAll
 
 rem only recalculate base values
-C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms LinkBenefits/calcBenefits/calcAllTrigger/Store_Operators
+rem C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms LinkBenefits/calcBenefits/calcAllTrigger/Store_Operators
+C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms LinkBenefits/calcBenefits/calcAllTrigger/Store_Base_Operators
 
 rem only simple evaluation (random operator!!!)
 rem C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms InvestmentDecision/SimpleDecisionEvaluator/Ran
