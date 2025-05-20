a6427ffeacdb9ea2095ec795f6863208545a8919
Chris Jacobs-Crisioni <chris@jacobs-crisioni.nl>
2025-05-20 10:24:17 +0200

    Further steps to maintain a consistent LinkTypes definition

--- batch/allocate_links.cmd
+++ batch/allocate_links.cmd
@@ -1,10 +1,10 @@
-rem C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms ChoiceSet/SelZones/Alternatives/ResultsCollection/allLinks 
+C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms ChoiceSet/SelZones/Alternatives/ResultsCollection/allLinks 
 
-rem C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms LinkBenefits/calcBenefits/calcAllTrigger/CalcAll
+C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms LinkBenefits/calcBenefits/calcAllTrigger/CalcAll
 
 rem only recalculate base values
 rem C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms LinkBenefits/calcBenefits/calcAllTrigger/Store_Operators
-C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms LinkBenefits/calcBenefits/calcAllTrigger/Store_Base_Operators
+rem C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms LinkBenefits/calcBenefits/calcAllTrigger/Store_Base_Operators
 
 rem only simple evaluation (random operator!!!)
 rem C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms InvestmentDecision/SimpleDecisionEvaluator/Ran
